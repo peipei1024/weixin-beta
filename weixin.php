272d8463ab47ef1f56ad7ec43823aac2c70f8500
pxz <1130397686@qq.com>
2016-12-06 17:36:54 +0800

    完成普通消息自动回复

--- weixin.php
+++ weixin.php
@@ -12,22 +12,83 @@
  * 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信。如果数据一致，返回$echostr
  */
 //获得参数 signature nonce token timestamp echostr
-$signature = $_GET['signature'];
-$timestamp = $_GET['timestamp'];
-$nonce     = $_GET['nonce'];
-$echostr   = $_GET['echostr'];
-$token     = 'weixin';
 
-//形成数组，然后按字典序排序
-$array = array();
-$array = array($nonce, $timestamp, $token);
-sort($array);
-//拼接成字符串,sha1加密 ，然后与signature进行校验
-$str = sha1( implode( $array ) );
-if( $str  == $signature && $echostr ){
-    //第一次接入weixin api接口的时候
-    echo  $echostr;
+
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->valid();
+$wechatObj->responseMsg();
+
+
+
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+
+            $postObj = simplexml_load_string($postStr);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>";
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+
+        }else {
+            echo "";
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
-
 
 ?>
